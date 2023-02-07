9f45153f8b48c5ac73358af1d7205521e309de58
steveJin-kmong <steve@kmong.com>
2023-02-07 18:58:16 +0900

    카운트다운 구현

--- countdown/index.js
+++ countdown/index.js
@@ -20,7 +20,7 @@ function init() {
 }
 
 function createRing() {
-  const PARTICLE_NUM = 20;
+  const PARTICLE_NUM = 1000;
   for (let i = 0; i < PARTICLE_NUM; i++) {
     particles.push(new Particle());
   }
@@ -35,10 +35,21 @@ function render() {
     now = Date.now();
     delta = now - then;
     if (delta < interval) return;
-    particles.forEach((particle, index) => {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    for (let i = particles.length - 1; 1 >= 0; i--) {
+      const particle = particles[i];
       particle.update();
       particle.draw(ctx);
-    });
+      if (particle.opacity < 0) particle.splice(index, 1);
+    }
+
+    // particles.forEach((particle, index) => {
+    //   particle.update();
+    //   particle.draw(ctx);
+
+    //   if (particle.opacity < 0) particle.splice(index, 1);
+    // });
 
     then = now - (delta % interval);
   };
@@ -54,5 +65,44 @@ window.addEventListener("resize", () => {
   init();
 });
 window.addEventListener("click", () => {
-  createRing();
+  const texts = document.querySelectorAll("span");
+  const countDownOption = {
+    opacity: 1,
+    scale: 1,
+    duration: 0.4,
+    ease: "Power4.easeOut",
+  };
+  gsap.fromTo(texts[0], { opacity: 0, scale: 5 }, { ...countDownOption });
+  gsap.fromTo(
+    texts[1],
+    { opacity: 0, scale: 5 },
+    {
+      ...countDownOption,
+      delay: 1,
+      onStart: () => (texts[0].style.opacity = 0),
+    }
+  );
+  gsap.fromTo(
+    texts[2],
+    { opacity: 0, scale: 5 },
+    {
+      ...countDownOption,
+      delay: 2,
+      onStart: () => (texts[1].style.opacity = 0),
+    }
+  );
+  const ringTag = document.querySelector("#ring");
+  gsap.fromTo(
+    ringTag,
+    { opacity: 1 },
+    {
+      opacity: 0,
+      duration: 1,
+      delay: 3,
+      onStart: () => {
+        createRing();
+        texts[2].style.opacity = 0;
+      },
+    }
+  );
 });
--- countdown/js/particle.js
+++ countdown/js/particle.js
@@ -2,20 +2,31 @@ import { randomNumBetween } from "./util.js";
 
 export default class Particle {
   constructor() {
+    this.rFriction = randomNumBetween(0.95, 1.01);
+    this.rAlpha = randomNumBetween(0, 5);
     this.r = innerHeight / 4;
-    this.angle = randomNumBetween(0, 360);
 
-    /**
-     * 중심점을 맞추는 것
-     */
+    this.angleFriction = randomNumBetween(0.97, 0.99);
+    this.angleAlpha = randomNumBetween(1, 2);
+    this.angle = randomNumBetween(0, 360);
+    this.opacity = randomNumBetween(0.2, 1);
+  }
+  update() {
+    this.rAlpha *= this.rFriction;
+    this.r += this.rAlpha;
+
+    this.angleAlpha *= this.angleFriction;
+    this.angle += this.angleAlpha; // 둥근 원으로 회전
+
     this.x = innerWidth / 2 + this.r * Math.cos((Math.PI / 180) * this.angle);
     this.y = innerHeight / 2 + this.r * Math.sin((Math.PI / 180) * this.angle);
+
+    this.opacity -= 0.003;
   }
-  update() {}
   draw(ctx) {
     ctx.beginPath();
     ctx.arc(this.x, this.y, 1, 0, Math.PI * 2);
-    ctx.fillStyle = "#fff";
+    ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
     ctx.fill();
     ctx.closePath();
   }
