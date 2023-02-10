6ad517e8b4de67b2157e18621d2bbc382e9a242f
steveJin-kmong <steve@kmong.com>
2023-02-10 23:11:46 +0900

    feature: 콘패티

--- confetti/index.js
+++ confetti/index.js
@@ -27,9 +27,9 @@ function init() {
   });
 }
 
-function confetti({ x, y, count, deg }) {
+function confetti({ x, y, count, deg, colors, shapes, spread }) {
   for (let i = 0; i < count; i++) {
-    particles.push(new Particle(x, y, deg));
+    particles.push(new Particle(x, y, deg, colors, shapes, spread));
   }
 }
 
@@ -37,43 +37,111 @@ function render() {
   let now, delta;
   let then = Date.now();
 
-  //   const x = innerWidth / 2;
-  //   let y = innerHeight / 2;
-  //   let widthAlpha = 0;
-
-  //   const width = 50;
-  //   const height = 50;
-  //   let deg = 0.1;
+  let deg = 0;
 
   const frame = () => {
     requestAnimationFrame(frame);
     now = Date.now();
     delta = now - then;
     if (delta < interval) return;
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    ctx.clearRect(0, 0, canvasWidth * 3, canvasHeight * 3);
+    /**
+     * 스쿨 퍼레이드
+     */
+    // confetti({
+    //   x: 0, // 0 ~ 1
+    //   y: 1, // 0 ~ 1
+    //   count: 10,
+    //   deg: -40,
+    // });
+    // confetti({
+    //   x: 1.99, // 0 ~ 1
+    //   y: 1, // 0 ~ 1
+    //   count: 10,
+    //   deg: -140,
+    // });
 
-    // widthAlpha += 0.1;
-    // deg += 0.1;
-    // y += 1;
+    /**
+     * 축하포 퍼레이드
+     */
+    // confetti({
+    //   x: 0, // 0 ~ 1
+    //   y: 0, // 0 ~ 1
+    //   count: 10,
+    //   deg: 45,
+    // });
+    // confetti({
+    //   x: 1.99, // 0 ~ 1
+    //   y: 0, // 0 ~ 1
+    //   count: 10,
+    //   deg: 135,
+    // });
 
-    // ctx.translate(x + width, y + height);
-    // ctx.rotate(deg);
-    // ctx.translate(-x - width, -y - height);
+    /**
+     * 가운데에서 위로
+     */
+    // confetti({
+    //   x: 1, // 0 ~ 1
+    //   y: 1, // 0 ~ 1
+    //   count: 10,
+    //   deg: 270,
+    // });
 
-    // ctx.fillStyle = "red";
-    // ctx.fillRect(
-    //   x,
-    //   y,
-    //   width * Math.cos(widthAlpha),
-    //   height * Math.sin(widthAlpha)
-    // );
-    // // ctx.fillRect(x, y, width, height);
+    /**
+     * 가운데에서 원으로
+     */
+    // confetti({
+    //   x: 1, // 0 ~ 1
+    //   y: 1, // 0 ~ 1
+    //   count: 10,
+    //   deg: 270,
+    //   spread: 180,
+    // });
 
-    // ctx.resetTransform();
+    /**
+     * 전체적으로 퍼지는 느낌
+     */
+    // confetti({
+    //   x: Math.random() + 0.5, // 0 ~ 1
+    //   y: Math.random() + 0.5, // 0 ~ 1
+    //   count: 10,
+    //   deg: 270,
+    //   spread: 180,
+    // });
+
+    /**
+     * 콘패티 회전
+     */
+    deg += 1;
+
+    confetti({
+      x: 1, // 0 ~ 1
+      y: 1, // 0 ~ 1
+      count: 10,
+      deg: 225 + deg,
+      spread: 1,
+    });
+    confetti({
+      x: 1, // 0 ~ 1
+      y: 1, // 0 ~ 1
+      count: 10,
+      deg: 90 + deg,
+      spread: 1,
+    });
+    confetti({
+      x: 1, // 0 ~ 1
+      y: 1, // 0 ~ 1
+      count: 10,
+      deg: 315 + deg,
+      spread: 1,
+    });
 
     for (let i = particles.length - 1; i >= 0; i--) {
       particles[i].update();
       particles[i].draw(ctx);
+
+      if (particles[i].opacity < 0) particles.splice(i, 1);
+      if (particles[i].y > canvasHeight * 2) particles.splice(i, 1);
     }
 
     then = now - (delta % interval);
@@ -90,9 +158,10 @@ window.addEventListener("load", () => {
 window.addEventListener("resize", init);
 window.addEventListener("click", () => {
   confetti({
-    x: 0,
-    y: canvasHeight / 2,
+    x: 0, // 0 ~ 1
+    y: 0.5, // 0 ~ 1
     count: 10,
     deg: -50,
+    spread: 1,
   });
 });
